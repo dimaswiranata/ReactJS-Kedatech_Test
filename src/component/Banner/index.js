bc5d9b5634f0d7a0cc44cd7cbdd5a0065d803fed
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-08-01 12:48:42 +0700

    add resposive and firebase hosting

--- src/component/Banner/index.js
+++ src/component/Banner/index.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './index.css';
 
 function Banner() {
+
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    
+    window.addEventListener("resize", handleResize);
+    
+    handleResize();
+    
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  
   return (
     <header
       className="banner"
       style={{
-        backgroundSize: 'cover',
+        backgroundSize: `cover`,
         backgroundImage: `url("https://firebasestorage.googleapis.com/v0/b/dummy-data-6e893.appspot.com/o/banner3.png?alt=media&token=281e440f-883d-40e8-88fd-4b19376eaf8a")`,
-        backgroundPosition: 'center center'
+        backgroundPosition: `${windowSize.width < 1000 ? "left center" : 'center center'}`, 
       }}
     >
       <div className="banner__section">
